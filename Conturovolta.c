84eb8ad0c4727c5a50d33c9f9bc51cf50f66c5dc
Conturovolta <Yusufdoner2005@gmail.com>
2026-05-26 23:08:48 +0300

    digit_Frequency-HR

--- Conturovolta.c
+++ Conturovolta.c
@@ -1,57 +1,26 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
 #include <string.h>
 
-void generate_Matrix(int size_of_matrix, int distance_of_orgin, int n);
+int main() {
+    int count[10] = {0}; 
+    char str[1001]; 
 
-int main() 
-{
-    int n;
-    
-    
-    if (scanf("%d", &n) != 1) {
-        return 1;
+    if (scanf("%[^\n]", str) != 1) {
+        return 0; 
     }
 
-    int size_of_matrix = (n * 2) - 1;
-    int distance_of_orgin = size_of_matrix / 2;
+   
+    int len = strlen(str); 
 
-    generate_Matrix(size_of_matrix, distance_of_orgin, n);
+    for (int i = 0; i < len; i++) {
+        if (str[i] >= '0' && str[i] <= '9') {
+            count[str[i] - '0']++; 
+        }
+    }
+
+    for (int i = 0; i < 10; i++) {
+        printf("%d ", count[i]);
+    }
 
     return 0;
-}
-
-void generate_Matrix(int size_of_matrix, int distance_of_orgin, int n)
-{
-    int k, l, i, j;
-    int x, y, max_uzaklik;
-    int matrix[size_of_matrix][size_of_matrix];
-
-    
-    for (k = 0; k < size_of_matrix; k++)
-    {
-        for (l = 0; l < size_of_matrix; l++)
-        {
-            x = abs(distance_of_orgin - k);
-            y = abs(distance_of_orgin - l);
-            
-            max_uzaklik = (x > y) ? x : y;
-            matrix[k][l] = max_uzaklik + 1;
-        }
-    }
-
-    
-    for (i = 0; i < size_of_matrix; i++)
-    {
-        for (j = 0; j < size_of_matrix; j++)
-        {
-            if (j == size_of_matrix - 1) {
-                printf("%d", matrix[i][j]);
-            } else {
-                printf("%d ", matrix[i][j]);
-            }
-        }
-        printf("\n"); 
-    }
 }
